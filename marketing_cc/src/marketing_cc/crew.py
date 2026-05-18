96a15ad0b356d31327661b77ab914e3f89e7a5ff
JayJatoba <jguilhermejatoba@hotmail.com>
2026-05-17 22:15:02 -0300

    initial commit

---
+++ marketing_cc/src/marketing_cc/crew.py
@@ -0,0 +1,48 @@
+from crewai import Agent, Crew, Process, Task
+from crewai.project import CrewBase, agent, crew, task
+from crewai.agents.agent_builder.base_agent import BaseAgent
+
+@CrewBase
+class MarketingCc():
+    """MarketingCc crew"""
+
+    agents: list[BaseAgent]
+    tasks: list[Task]
+
+    @agent
+    def researcher(self) -> Agent:
+        return Agent(
+            config=self.agents_config['researcher'],
+            verbose=True
+        )
+
+    @agent
+    def reporting_analyst(self) -> Agent:
+        return Agent(
+            config=self.agents_config['reporting_analyst'],
+            verbose=True
+        )
+
+    @task
+    def research_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['research_task'],
+        )
+
+    @task
+    def reporting_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['reporting_task'],
+            output_file='report.md'
+        )
+
+    @crew
+    def crew(self) -> Crew:
+        """Creates the MarketingCc crew"""
+
+        return Crew(
+            agents=self.agents, 
+            tasks=self.tasks, 
+            process=Process.sequential,
+            verbose=True,
+        )
